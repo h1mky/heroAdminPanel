81283bae628ef027455df367333b727e56132876
zvezd <adelevnikita@gmail.com>
2025-04-06 01:29:47 +0300

    optimize code

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -3,18 +3,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import classNames from "classnames";
 
-import {
-  filtersFetched,
-  filtersFetching,
-  filtersFetchingError,
-  activeFilterChanged,
-} from "./filtersSlice";
+import { activeFilterChanged, fetchFilters } from "./filtersSlice";
 import Spinner from "../spinner/Spinner";
-
-// Задача для этого компонента:
-// Фильтры должны формироваться на основании загруженных данных
-// Фильтры должны отображать только нужных героев при выборе
-// Активный фильтр имеет класс active
 
 const HeroesFilters = () => {
   const { filters, filtersLoadingStatus, activeFilter } = useSelector(
@@ -23,12 +13,8 @@ const HeroesFilters = () => {
   const dispatch = useDispatch();
   const { request } = useHttp();
 
-  // Запрос на сервер для получения фильтров и последовательной смены состояния
   useEffect(() => {
-    dispatch(filtersFetching());
-    request("http://localhost:3001/filters")
-      .then((data) => dispatch(filtersFetched(data)))
-      .catch(() => dispatch(filtersFetchingError()));
+    dispatch(fetchFilters());
 
     // eslint-disable-next-line
   }, []);
@@ -44,7 +30,6 @@ const HeroesFilters = () => {
       return <h5 className="text-center mt-5">Фильтры не найдены</h5>;
     }
 
-    // Данные в json-файле я расширил классами и текстом
     return arr.map(({ name, className, label }) => {
       // Используем библиотеку classnames и формируем классы динамически
       const btnClass = classNames("btn", className, {
