adc061ec09eb1aafc7bc1ed7def73e6a44fb341d
zvezd <adelevnikita@gmail.com>
2025-04-08 12:38:06 +0300

    delete rubish

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,16 +1,12 @@
 import { useCallback } from "react";
 
 export const useHttp = () => {
-  // const [process, setProcess] = useState('waiting');
-
   const request = async (
     url,
     method = "GET",
     body = null,
     headers = { "Content-Type": "application/json" }
   ) => {
-    // setProcess('loading');
-
     try {
       const response = await fetch(url, { method, body, headers });
 
@@ -22,19 +18,11 @@ export const useHttp = () => {
 
       return data;
     } catch (e) {
-      // setProcess('error');
       throw e;
     }
   };
 
-  // const clearError = useCallback(() => {
-  // setProcess('loading');
-  // }, []);
-
   return {
     request,
-    // clearError,
-    // process,
-    // setProcess
   };
 };
